896ad9b5b60006a394a354fe42f8f1a1eebe6acf
ugursabirer <sabirerugur@gmail.com>
2022-02-24 14:20:47 +0300

    first commit

---
+++ bin/dart_variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String mesaj = "Merhaba Uğur";
+  int dogumYili = 2000;
+  double oran = 1.15;
+  bool girisYaptiMi = false;
+
+  print(mesaj);
+  print("Doğum yılı: " + dogumYili.toString());
+  print("Oran: " + oran.toString());
+  print(girisYaptiMi);
+}
